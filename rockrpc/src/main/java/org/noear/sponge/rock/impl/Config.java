3bda945636176c03d820f96f6d3f12ba4e5e346d
noear <noear@live.cn>
2021-01-07 18:07:10 +0800

    1.0.1

--- rockrpc/src/main/java/org/noear/sponge/rock/impl/Config.java
+++ rockrpc/src/main/java/org/noear/sponge/rock/impl/Config.java
@@ -13,12 +13,12 @@ public class Config {
 
     @Bean("rock_db")
     public DbContext rock_db() {
-        return cfg("rock").getDb(true);
+        return cfg("sponge_rock").getDb(true);
     }
 
     @Bean("rock_cache")
     public ICacheServiceEx rock_cache() {
-        return cfg("rock_cache").getCh2().nameSet("rock_cache");
+        return cfg("sponge_cache").getCh2().nameSet("rock_cache");
     }
 
     @Bean("rock_log")
