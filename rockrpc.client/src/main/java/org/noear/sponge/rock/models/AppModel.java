94cb56831ea4c42a21b56c59e4995bb168e08d44
noear <noear@live.cn>
2021-01-13 15:03:36 +0800

    1.0.1

--- rockrpc.client/src/main/java/org/noear/sponge/rock/models/AppModel.java
+++ rockrpc.client/src/main/java/org/noear/sponge/rock/models/AppModel.java
@@ -12,8 +12,7 @@ import java.util.Map;
 /// 生成:2017/05/25 09:41:35
 /// 
 /// </summary>
-public class AppModel implements IBinder
-{
+public class AppModel implements IBinder {
     public static transient RockRpc rockclient;
 
     public int app_id;
@@ -30,10 +29,9 @@ public class AppModel implements IBinder
     public int ar_examine_ver;
 
     @Override
-	public void bind(GetHandlerEx s)
-	{
-		//1.source:数据源
-		//
+    public void bind(GetHandlerEx s) {
+        //1.source:数据源
+        //
         app_id = s.get("app_id").value(0);
         app_key = s.get("app_key").value(null);
         akey = s.get("akey").value(null);
@@ -46,27 +44,30 @@ public class AppModel implements IBinder
 
         ar_is_examine = s.get("ar_is_examine").value(0);
         ar_examine_ver = s.get("ar_examine_ver").value(0);
-	}
+    }
 
     @Override
-	public IBinder clone()
-	{
-		return new AppModel();
-	}
+    public IBinder clone() {
+        return new AppModel();
+    }
 
     //是否为审核中
 
-	//=============
+    //=============
     //
     //
 
-	//获取对应的应用组
+    /**
+     * 获取对应的应用组
+     * */
     @JSONField(serialize = false)
-	public AppGroupModel agroup() throws Exception {
+    public AppGroupModel agroup() throws Exception {
         return rockclient.getAppGroup(agroup_id);
     }
 
-    //获取对应的用户组
+    /**
+     * 获取对应的用户组
+     * */
     @JSONField(serialize = false)
     public UserGroupModel ugroup() throws Exception {
         return rockclient.getUserGroup(ugroup_id);
@@ -75,46 +76,57 @@ public class AppModel implements IBinder
     //=============
     //
     //
-    private Map<Integer,AppSettingCollection> _setting = null;
-	private void tryInitSetting(int ver) throws Exception {
-	    if(_setting == null){
+    private Map<Integer, AppSettingCollection> _setting = null;
+
+    private void tryInitSetting(int ver) throws Exception {
+        if (_setting == null) {
             _setting = new HashMap<>();
         }
 
-        if(_setting.containsKey(ver) == false){
-            _setting.put(ver, rockclient.getAppSettingEx(app_id, ver, false));
+        if (_setting.containsKey(ver) == false) {
+            _setting.put(ver, rockclient.getAppSettingEx2(agroup_id, app_id, ver, false));
         }
     }
-    private transient Map<Integer,AppSettingCollection> _client_setting = null;
+
+    private transient Map<Integer, AppSettingCollection> _client_setting = null;
+
     private void tryInitClientSetting(int ver) throws Exception {
-        if(_client_setting == null){
+        if (_client_setting == null) {
             _client_setting = new HashMap<>();
 
         }
         if (_client_setting.containsKey(ver) == false) {
-            _client_setting.put(ver, rockclient.getAppSettingEx(app_id, ver, true));
+            _client_setting.put(ver, rockclient.getAppSettingEx2(agroup_id, app_id, ver, true));
         }
     }
 
 
-    //获取所有设置集合
+    /**
+     * 获取所有设置集合
+     * */
     @JSONField(serialize = false)
-	public AppSettingCollection getSetting(int ver) throws Exception {
+    public AppSettingCollection getSetting(int ver) throws Exception {
         tryInitSetting(ver);
 
         return _setting.get(ver);
     }
 
 
-	//获取客户端的设置集合
+    /**
+     * 获取客户端的设置集合
+     * */
     @JSONField(serialize = false)
-    public  AppSettingCollection getClientSetting(int ver) throws Exception {
+    public AppSettingCollection getClientSetting(int ver) throws Exception {
         tryInitClientSetting(ver);
 
         return _client_setting.get(ver);
     }
 
     private transient AppVersionModel _ver;
+
+    /**
+     * 获取一个版本号
+     * */
     @JSONField(serialize = false)
     public AppVersionModel getVersion(int platform) throws Exception {
         if (_ver == null) {
@@ -124,14 +136,16 @@ public class AppModel implements IBinder
         return _ver;
     }
 
-    //获取一个设置项
+    /**
+     * 获取一个设置项
+     * */
     @JSONField(serialize = false)
-    public AppSettingModel get(String key) throws Exception {
+    public AppSettingModel getSetting(String key) throws Exception {
         tryInitSetting(0);
 
-        if(_setting.get(0).containsKey(key)){
-           return _setting.get(0).get(key);
-        }else {
+        if (_setting.get(0).containsKey(key)) {
+            return _setting.get(0).get(key);
+        } else {
             return null;
         }
     }
