6feb554faa11af4bfac2a4a8ee7ca7e05ba1bf69
noear <noear@live.cn>
2021-01-25 16:10:24 +0800

    1.0.1

--- rock.client/src/main/java/org/noear/sponge/rock/models/AppSettingModel.java
+++ rock.client/src/main/java/org/noear/sponge/rock/models/AppSettingModel.java
@@ -1,7 +1,7 @@
 package org.noear.sponge.rock.models;
 
 import org.noear.snack.ONode;
-import org.noear.water.utils.RunUtils;
+import org.noear.water.utils.CallUtils;
 import org.noear.weed.GetHandlerEx;
 import org.noear.weed.IBinder;
 
@@ -12,85 +12,81 @@ import java.util.Properties;
 /// 生成:2017/05/25 09:41:35
 /// 
 /// </summary>
-public class AppSettingModel implements IBinder
-{
+public class AppSettingModel implements IBinder {
 	public int row_id;
 	public int agroup_id;
 	public int app_id;
 	public int is_client;
-    public String name;
-    //设置项值类型：0,文本；1,数字; 9,JSON
-    public int type;
-    public String value;
-    public int ver_start;
+	public String name;
+	//设置项值类型：0,文本；1,数字; 9,JSON
+	public int type;
+	public String value;
+	public int ver_start;
 
-	public void bind(GetHandlerEx s)
-	{
+	public void bind(GetHandlerEx s) {
 		//1.source:数据源
 		//
 		row_id = s.get("row_id").value(0);
 		agroup_id = s.get("agroup_id").value(0);
 		app_id = s.get("app_id").value(0);
 		is_client = s.get("is_client").value(0);
-        name = s.get("name").value(null);
+		name = s.get("name").value(null);
 		type = s.get("type").value(0);
-        value = s.get("value").value(null);
-        ver_start= s.get("ver_start").value(0);
+		value = s.get("value").value(null);
+		ver_start = s.get("ver_start").value(0);
 	}
-	
-	public IBinder clone()
-	{
+
+	public IBinder clone() {
 		return new AppSettingModel();
 	}
 
 
 	//获取数字类型的值
-	public long getLong(){
-		if(type == 1) {
+	public long getLong() {
+		if (type == 1) {
 			return Long.parseLong(value);
-		}else{
+		} else {
 			return 0;
 		}
 	}
 
 	//获取数字类型的值
-	public int getInt(){
-		if(type == 1) {
+	public int getInt() {
+		if (type == 1) {
 			return Integer.parseInt(value);
-		}else{
+		} else {
 			return 0;
 		}
 	}
 
 
 	//获取JSON类型的值里的属性
-	public ONode get(String name){
-		if(_node == null){
+	public ONode get(String name) {
+		if (_node == null) {
 			_node = getNode();
 		}
 
 		return _node.get(name);
 	}
+
 	private ONode _node;
 
 	//转为JSON对象
-	public ONode getNode(){
-		if(type==1){
-			if(value == null || value.length() == 0)
+	public ONode getNode() {
+		if (type == 1) {
+			if (value == null || value.length() == 0)
 				return new ONode().val(0);
 			else {
-			    if(value.indexOf(".")>0) {
+				if (value.indexOf(".") > 0) {
 
-                    return new ONode().val(Double.parseDouble(value));
-                }
-			    else{
-                    return new ONode().val(Integer.parseInt(value));
-                }
-            }
-		}
-		else if(type==9){
+					return new ONode().val(Double.parseDouble(value));
+				} else {
+					return new ONode().val(Integer.parseInt(value));
+				}
+			}
+		} else if (type == 9) {
 			return ONode.load(value);
-		}else{
+		} else {
 			return new ONode().val(value);
 		}
 	}
@@ -99,10 +95,11 @@ public class AppSettingModel implements IBinder
 	 * 转为Properties
 	 */
 	private transient Properties _prop;
+
 	public Properties getProp() {
 		if (_prop == null) {
 			_prop = new Properties();
-			RunUtils.runActEx(() -> _prop.load(new StringReader(value)));
+			CallUtils.run(() -> _prop.load(new StringReader(value)));
 		}
 
 		return _prop;
