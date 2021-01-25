06300a18fe5800ec890340f007003e01c3686457
noear <noear@live.cn>
2021-01-25 17:53:52 +0800

    1.0.1

--- rockrpc/src/main/java/org/noear/sponge/rock/impl/App.java
+++ rockrpc/src/main/java/org/noear/sponge/rock/impl/App.java
@@ -6,7 +6,6 @@ import org.noear.sponge.rock.impl.controller.event.err_event;
 
 public class App {
     public static void main(String[] args) {
-        Solon.start(App.class, args)
-                .onError(Aop.get(err_event.class));
+        Solon.start(App.class, args);
     }
 }
