82899a765fd2453ebde4f451e40321ed1037b428
noear <noear@live.cn>
2021-02-06 22:29:17 +0800

    1.0.2

--- rockrpc/src/main/java/org/noear/sponge/rock/impl/App.java
+++ rockrpc/src/main/java/org/noear/sponge/rock/impl/App.java
@@ -5,7 +5,6 @@ import org.noear.solon.Solon;
 public class App {
     public static void main(String[] args) {
         Solon.start(App.class, args, app ->
-                app.enableSocketD(true)
-                        .enableSafeStop(app.cfg().isFilesMode() == false));
+                app.enableSafeStop(app.cfg().isFilesMode() == false));
     }
 }
