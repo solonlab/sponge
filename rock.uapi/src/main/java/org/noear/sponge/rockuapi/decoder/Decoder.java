e3cfa266df0e91ea8c5cef6f425c839a595eea12
noear <noear@live.cn>
2021-02-14 18:28:17 +0800

    1.0.2

--- rock.uapi/src/main/java/org/noear/sponge/rockuapi/decoder/Decoder.java
+++
@@ -1,8 +0,0 @@
-package org.noear.sponge.rockuapi.decoder;
-
-import org.noear.sponge.rock.models.AppModel;
-import org.noear.solon.core.handle.Context;
-
-public interface Decoder {
-    String tryDecode(Context context, AppModel app, String text)  throws Exception;
-}
