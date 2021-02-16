13aae34469919d60445f603cbfa5b9f6c6b7b98f
noear <noear@live.cn>
2021-02-16 13:21:46 +0800

    1.0.2

--- spongeadmin/src/main/java/org/noear/sponge/admin/dso/CacheUtil.java
+++ spongeadmin/src/main/java/org/noear/sponge/admin/dso/CacheUtil.java
@@ -1,12 +1,13 @@
 package org.noear.sponge.admin.dso;
 
+import org.noear.solon.Solon;
 import org.noear.weed.cache.ICacheServiceEx;
 import org.noear.sponge.admin.Config;
 
 public class CacheUtil {
    public static boolean isUsingCache = true;
 
-   public static ICacheServiceEx dataCache = Config.cfg("rock_cache").getCh(Config.water_service_name + "_DATA_CACHE", 60 * 5); // new EmptyCache();
+   public static ICacheServiceEx dataCache = Config.cfg("rock_cache").getCh(Solon.cfg().appName() + "_DATA_CACHE", 60 * 5); // new EmptyCache();
 
    public static void clearByTag(String tag) {
       dataCache.clear(tag);
