691926671ced54d4078558aea8e4e7dfa51c6c9c
noear <noear@live.cn>
2021-02-26 20:45:01 +0800

    1.0.5

---
+++ rock.client/src/main/java/org/noear/rock/utils/I18nContext.java
@@ -0,0 +1,97 @@
+package org.noear.rock.utils;
+
+import org.noear.rock.RockClient;
+import org.noear.rock.model.AppCodeCollection;
+import org.noear.rock.model.AppI18nCollection;
+import org.noear.water.utils.TaskUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 国际化上下文（其实例必须静态化）
+ *
+ * @author noear 2021/2/26 created
+ */
+public class I18nContext {
+    static Logger log = LoggerFactory.getLogger(I18nContext.class);
+
+    Map<String, AppCodeCollection> codeMap = new HashMap<>();
+    Map<String, AppI18nCollection> nameMap = new HashMap<>();
+    String service;
+
+    public I18nContext(String service) {
+        this.service = service;
+        TaskUtils.run(10 * 1000, 10 * 1000, this::update);
+    }
+
+    public String getByCode(int code, String lang) throws SQLException {
+        if (lang == null) {
+            lang = "";
+        }
+
+        AppCodeCollection coll = codeMap.get(lang);
+        if (coll == null) {
+            synchronized (lang.intern()) {
+                coll = codeMap.get(lang);
+                if (coll == null) {
+                    coll = RockClient.getServiceCodesByLang(service, lang);
+                    codeMap.put(lang, coll);
+                }
+            }
+        }
+
+        return coll.get(code);
+    }
+
+    public String getByName(String name, String lang) throws SQLException {
+        if (lang == null) {
+            lang = "";
+        }
+
+        AppI18nCollection coll = nameMap.get(lang);
+        if (coll == null) {
+            synchronized (lang.intern()) {
+                coll = nameMap.get(lang);
+                if (coll == null) {
+                    coll = RockClient.getServiceI18nsByLang(service, lang);
+                    nameMap.put(lang, coll);
+                }
+            }
+        }
+
+        return coll.get(name);
+    }
+
+    protected void updateCodeMap() throws SQLException {
+        Map<String, AppCodeCollection> codeMap2 = new HashMap<>();
+
+        for (Map.Entry<String, AppCodeCollection> kv : codeMap.entrySet()) {
+            AppCodeCollection coll = RockClient.getServiceCodesByLang(service, kv.getKey());
+            codeMap2.put(kv.getKey(), coll);
+        }
+
+        codeMap = codeMap2;
+    }
+
+    protected void updateNameMap() throws SQLException {
+        Map<String, AppI18nCollection> nameMap2 = new HashMap<>();
+
+        for (Map.Entry<String, AppI18nCollection> kv : nameMap.entrySet()) {
+            AppI18nCollection coll = RockClient.getServiceI18nsByLang(service, kv.getKey());
+            nameMap2.put(kv.getKey(), coll);
+        }
+
+        nameMap = nameMap2;
+    }
+
+    protected void update() throws SQLException {
+        updateCodeMap();
+        updateNameMap();
+
+        log.debug("{}: {}", service, "i18n context update succeed!");
+    }
+}
--- rock.client/src/main/java/org/noear/rock/utils/I18nUtils.java
+++ rock.client/src/main/java/org/noear/rock/utils/I18nUtils.java
@@ -1,83 +1,32 @@
 package org.noear.rock.utils;
 
-import org.noear.rock.RockClient;
-import org.noear.rock.model.AppCodeCollection;
-import org.noear.rock.model.AppI18nCollection;
 import org.noear.solon.Solon;
-import org.noear.water.utils.TaskUtils;
 
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author noear 2021/2/24 created
  */
 public class I18nUtils {
-    static Map<String, AppCodeCollection> codeMap = new HashMap<>();
-    static Map<String, AppI18nCollection> nameMap = new HashMap<>();
+    static I18nContext context;
 
-    static {
-        TaskUtils.run(new I18nTask());
+    private static I18nContext getContext() {
+        if (context == null) {
+            synchronized (I18nUtils.context) {
+                if (context == null) {
+                    context = new I18nContext(Solon.cfg().appName());
+                }
+            }
+        }
+
+        return context;
     }
 
     public static String getByCode(int code, String lang) throws SQLException {
-        if (lang == null) {
-            lang = "";
-        }
-
-        AppCodeCollection coll = codeMap.get(lang);
-        if (coll == null) {
-            synchronized (lang.intern()) {
-                coll = codeMap.get(lang);
-                if (coll == null) {
-                    coll = RockClient.getServiceCodesByLang(Solon.cfg().appName(), lang);
-                    codeMap.put(lang, coll);
-                }
-            }
-        }
-
-        return coll.get(code);
+        return getContext().getByCode(code, lang);
     }
 
     public static String getByName(String name, String lang) throws SQLException {
-        if (lang == null) {
-            lang = "";
-        }
-
-        AppI18nCollection coll = nameMap.get(lang);
-        if (coll == null) {
-            synchronized (lang.intern()) {
-                coll = nameMap.get(lang);
-                if (coll == null) {
-                    coll = RockClient.getServiceI18nsByLang(Solon.cfg().appName(), lang);
-                    nameMap.put(lang, coll);
-                }
-            }
-        }
-
-        return coll.get(name);
-    }
-
-    protected static void updateCodeMap() throws SQLException {
-        Map<String, AppCodeCollection> codeMap2 = new HashMap<>();
-
-        for (Map.Entry<String, AppCodeCollection> kv : codeMap.entrySet()) {
-            AppCodeCollection coll = RockClient.getServiceCodesByLang(Solon.cfg().appName(), kv.getKey());
-            codeMap2.put(kv.getKey(), coll);
-        }
-
-        codeMap = codeMap2;
-    }
-
-    protected static void updateNameMap() throws SQLException {
-        Map<String, AppI18nCollection> nameMap2 = new HashMap<>();
-
-        for (Map.Entry<String, AppI18nCollection> kv : nameMap.entrySet()) {
-            AppI18nCollection coll = RockClient.getServiceI18nsByLang(Solon.cfg().appName(), kv.getKey());
-            nameMap2.put(kv.getKey(), coll);
-        }
-
-        nameMap = nameMap2;
+        return getContext().getByName(name, lang);
     }
 }
