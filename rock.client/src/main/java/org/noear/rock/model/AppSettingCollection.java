bd6a75cde454d369296f1b081c22571010fc4e29
noear <noear@live.cn>
2021-03-07 16:24:28 +0800

    1.0.6

--- rock.client/src/main/java/org/noear/rock/model/AppSettingCollection.java
+++ rock.client/src/main/java/org/noear/rock/model/AppSettingCollection.java
@@ -1,16 +1,27 @@
 package org.noear.rock.model;
 
-
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class AppSettingCollection extends HashMap<String, AppSettingModel> implements Serializable {
+public class AppSettingCollection implements Serializable {
+    public Map<String, AppSettingModel> data = new HashMap<>();
 
-    public void bind(List<AppSettingModel> mod){
-        for(AppSettingModel m:mod){
-            this.put(m.name,m);
+    public void bind(List<AppSettingModel> mod) {
+        for (AppSettingModel m : mod) {
+            data.put(m.name, m);
         }
     }
 
+    public AppSettingModel get(String name){
+        return data.get(name);
+    }
+
+    @Override
+    public String toString() {
+        return "AppSettingCollection{" +
+                "data=" + data +
+                '}';
+    }
 }
