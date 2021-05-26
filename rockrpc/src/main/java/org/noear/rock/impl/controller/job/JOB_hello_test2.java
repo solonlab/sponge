c2079d78e47244e78bd0ca43c9e80482411cf662
noear <noear@live.cn>
2021-05-26 21:46:01 +0800

    1.0.11

---
+++ rockrpc/src/main/java/org/noear/rock/impl/controller/job/JOB_hello_test2.java
@@ -0,0 +1,18 @@
+package org.noear.rock.impl.controller.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.noear.solon.cloud.annotation.CloudJob;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Handler;
+
+/**
+ * @author noear 2021/5/26 created
+ */
+@Slf4j
+@CloudJob(value = "hello_test2", description = "异常测试")
+public class JOB_hello_test2 implements Handler {
+    @Override
+    public void handle(Context ctx) throws Throwable {
+        throw new IllegalArgumentException("没有参数");
+    }
+}
