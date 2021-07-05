826cb3155c185ef0031ca5c01ee54e5179d19296
noear <noear@live.cn>
2021-07-05 16:41:31 +0800

    1.1.2

--- db_log/sponge_rock.sql
+++ db_log/sponge_rock.sql
@@ -4,3 +4,7 @@ ALTER TABLE `appx_ex_i18n`
 
 ALTER TABLE `appx_ex_code`
     ADD COLUMN `is_disabled` int(1) NOT NULL DEFAULT 0 COMMENT '禁用' AFTER `note`;
+
+
+ALTER TABLE `appx_ex_setting`
+    ADD COLUMN `is_disabled` int(1) NOT NULL DEFAULT 0 COMMENT '禁用' AFTER `note`;
