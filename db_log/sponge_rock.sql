103ce72f3241ed7230566a09ab6432c13fc5a246
noear <noear@live.cn>
2021-09-22 22:14:33 +0800

    1.2.1

--- db_log/sponge_rock.sql
+++ db_log/sponge_rock.sql
@@ -17,3 +17,7 @@ ALTER TABLE `appx_ex_setting`
 
 ALTER TABLE `appx`
     ADD COLUMN `is_disabled` int(1) NOT NULL DEFAULT 0 COMMENT '禁用' AFTER `note`;
+
+-- 2021.09.22
+ALTER TABLE `appx_ex_code`
+    MODIFY COLUMN `lang` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' AFTER `code`;
