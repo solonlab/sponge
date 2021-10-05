78ca3c07b9af68c0aa4c2f09def0d6fc622cc5da
noear <noear@live.cn>
2021-10-05 13:51:54 +0800

    1.2.3

--- trackapi/src/main/java/org/noear/trackapi/App.java
+++ trackapi/src/main/java/org/noear/trackapi/App.java
@@ -1,8 +1,6 @@
 package org.noear.trackapi;
 
 import org.noear.solon.Solon;
-import org.noear.solon.core.handle.Context;
-import org.noear.trackapi.dso.LogUtil;
 
 public class App {
     public static void main(String[] args) {
