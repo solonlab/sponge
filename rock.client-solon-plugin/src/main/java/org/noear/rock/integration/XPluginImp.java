60abc3813082fc47c98a033800f63ae0dbde5b75
noear <noear@live.cn>
2021-10-29 22:58:41 +0800

    1.2.9

--- rock.client-solon-plugin/src/main/java/org/noear/rock/integration/XPluginImp.java
+++ rock.client-solon-plugin/src/main/java/org/noear/rock/integration/XPluginImp.java
@@ -1,7 +1,10 @@
 package org.noear.rock.integration;
 
+import org.noear.rock.solon.I18nBundleFactoryImpl;
 import org.noear.solon.SolonApp;
+import org.noear.solon.core.Aop;
 import org.noear.solon.core.Plugin;
+import org.noear.solon.i18n.I18nBundleFactory;
 
 /**
  * @author noear 2021/10/29 created
@@ -10,5 +13,6 @@ public class XPluginImp implements Plugin {
     @Override
     public void start(SolonApp app) {
         app.beanMake(msg_updatecache.class);
+        Aop.wrapAndPut(I18nBundleFactory.class, new I18nBundleFactoryImpl());
     }
 }
