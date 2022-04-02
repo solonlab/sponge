5768dbd4f34694e05abd4f8e5f42dc5e07f0b567
noear <noear@live.cn>
2022-04-02 18:32:37 +0800

    2.0.6

--- rockrpc/src/main/java/org/noear/rock/impl/controller/job/JOB_hello_test.java
+++ rockrpc/src/main/java/org/noear/rock/impl/controller/job/JOB_hello_test.java
@@ -1,16 +1,16 @@
 package org.noear.rock.impl.controller.job;
 
 import lombok.extern.slf4j.Slf4j;
+import org.noear.solon.cloud.CloudJobHandler;
 import org.noear.solon.cloud.annotation.CloudJob;
 import org.noear.solon.core.handle.Context;
-import org.noear.solon.core.handle.Handler;
 
 /**
  * @author noear 2021/5/26 created
  */
 @Slf4j
 @CloudJob(value = "hello_test", cron7x = "10s")
-public class JOB_hello_test implements Handler {
+public class JOB_hello_test implements CloudJobHandler {
     @Override
     public void handle(Context ctx) throws Throwable {
         log.info("exe job: hello_test");
--- rockrpc/src/main/java/org/noear/rock/impl/controller/job/JOB_hello_test2.java
+++ rockrpc/src/main/java/org/noear/rock/impl/controller/job/JOB_hello_test2.java
@@ -1,17 +1,16 @@
 package org.noear.rock.impl.controller.job;
 
 import lombok.extern.slf4j.Slf4j;
-import org.noear.snack.ONode;
+import org.noear.solon.cloud.CloudJobHandler;
 import org.noear.solon.cloud.annotation.CloudJob;
 import org.noear.solon.core.handle.Context;
-import org.noear.solon.core.handle.Handler;
 
 /**
  * @author noear 2021/5/26 created
  */
 @Slf4j
 @CloudJob(value = "hello_test2", cron7x = "0 * * * * ? *", description = "异常测试")
-public class JOB_hello_test2 implements Handler {
+public class JOB_hello_test2 implements CloudJobHandler {
     @Override
     public void handle(Context ctx) throws Throwable {
         throw new IllegalArgumentException("异常测试");
