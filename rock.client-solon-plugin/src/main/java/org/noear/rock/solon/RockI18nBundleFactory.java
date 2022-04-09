94bbe8413c14d4aa05d4c2b367e90e16c93df9a0
noear <noear@live.cn>
2022-04-09 10:35:49 +0800

    2.1.0

--- rock.client-solon-plugin/src/main/java/org/noear/rock/solon/RockI18nBundleFactory.java
+++ rock.client-solon-plugin/src/main/java/org/noear/rock/solon/RockI18nBundleFactory.java
@@ -9,25 +9,15 @@ import org.noear.solon.i18n.I18nUtil;
 import java.util.Locale;
 
 /**
- * 适配 solon.i18n 规范（）
+ * 适配 solon.i18n 规范
  *
- * @author noear 2022/4/8 created
+ * @author noear 2021/9/8 created
  */
 public class RockI18nBundleFactory implements I18nBundleFactory {
-    private final String codeBundleNameDef;
-
-    public RockI18nBundleFactory() {
-        codeBundleNameDef = Solon.cfg().appName() + "_code";
-    }
-
     @Override
     public I18nBundle create(String bundleName, Locale locale) {
         if (I18nUtil.getMessageBundleName().equals(bundleName)) {
             bundleName = Solon.cfg().appName();
-        }
-
-        if (bundleName.equals(codeBundleNameDef)) {
-            return new RockCodeI18nBundle(I18nContextManager.getCodeContext(bundleName), locale);
         }
 
         return new RockI18nBundle(I18nContextManager.getMessageContext(bundleName), locale);
