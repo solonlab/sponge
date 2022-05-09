37471aed9070b3c8bb2c483cd90cbf32deea374d
noear <noear@live.cn>
2022-05-09 22:00:35 +0800

    2.1.4

--- rockrpc/src/main/java/org/noear/rock/impl/controller/RpcGateway.java
+++ rockrpc/src/main/java/org/noear/rock/impl/controller/RpcGateway.java
@@ -1,15 +1,14 @@
 package org.noear.rock.impl.controller;
 
-import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.core.handle.Gateway;
-import org.noear.solon.core.handle.MethodType;
 import org.noear.rock.impl.controller.trigger.EndHandler;
 import org.noear.rock.impl.controller.trigger.IpHandler;
 import org.noear.rock.impl.controller.trigger.StartHandler;
 
-@Mapping(value = "/*", method = MethodType.ALL)
-@Controller
+@Mapping("/*")
+@Component
 public class RpcGateway extends Gateway {
     @Override
     protected void register() {
