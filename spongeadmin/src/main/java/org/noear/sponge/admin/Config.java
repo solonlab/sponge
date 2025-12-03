57444351b56ccccbbd96990afdbb840dd1809c22
noear <noear@live.cn>
2025-12-03 21:45:18 +0800

    2.14.1

--- spongeadmin/src/main/java/org/noear/sponge/admin/Config.java
+++ spongeadmin/src/main/java/org/noear/sponge/admin/Config.java
@@ -19,7 +19,7 @@ import java.util.Map;
 @Configuration
 public class Config {
 
-    public static final String sponge_version = "v2.14.0";
+    public static final String sponge_version = "v2.14.1";
 
     public static String track_uri() {
         return cfg("track_uri").value;
